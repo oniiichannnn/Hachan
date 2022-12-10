a0a6c813f70c4af0b41f16bc78419a35ad590ad2
oniichan <wolfguyplays6@gmail.com>
2022-12-10 17:09:25 +0800

    added [0]

--- PushToServer.js
+++ PushToServer.js
@@ -20,6 +20,7 @@ fs.writeFileSync("./ImageData.json", JSON.stringify(WriteData, null, 2));
 
 const NewlyAddedImages = Object.keys(WriteData).filter(imagedir => imagedir in oldFiles === false);
 
+console.log("Newly Added", NewlyAddedImages);
 
 const command = `git add . && git commit -m \"added [${NewlyAddedImages.length}] ${NewlyAddedImages.join(", ")}\" && git push -u origin main`;
 
